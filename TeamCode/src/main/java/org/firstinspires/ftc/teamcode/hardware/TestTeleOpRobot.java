04f18c8ce9728a98463c6902bed40507a58966e9
Zach Harel <zach.harel42@gmail.com>
2024-10-22 21:46:42 -0400

    added the PIDF controller and changed testteleop to use the LinearSlides class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TestTeleOpRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/TestTeleOpRobot.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.hardware.mechanisms.LinearSlides;
 import org.firstinspires.ftc.teamcode.hardware.wrappers.Motor;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
@@ -13,25 +14,34 @@ public class TestTeleOpRobot {
     private HardwareMap hwMap;
     private Gamepad gp1;
     private MecanumDrive drive;
-    private Motor slides,arm;
+    private Motor arm;
+    private LinearSlides slides;
 
     public TestTeleOpRobot(HardwareMap hwMap, Gamepad gp1) {
         this.hwMap = hwMap;
         this.gp1 = gp1;
         drive = new MecanumDrive(hwMap, new Pose2d(0.0,0.0,0.0));
-        slides = new Motor("slides",hwMap);
+        slides = new LinearSlides(new Motor("slides",hwMap));
         arm = new Motor("arm",hwMap);
+    }
+
+    public TestTeleOpRobot(HardwareMap hwMap, Gamepad gp1, MecanumDrive drive, LinearSlides slides, Motor arm) {
+        this.hwMap = hwMap;
+        this.gp1 = gp1;
+        this.drive = drive;
+        this.slides = slides;
+        this.arm = arm;
     }
 
     public void slides() {
         if(gp1.a) {
-            slides.getDcMotorEx().setPower(1);
+            slides.setPower(1);
         }
         else if(gp1.b) {
-            slides.getDcMotorEx().setPower(-1);
+            slides.setPower(-1);
         }
         else {
-            slides.getDcMotorEx().setPower(0);
+            slides.setPower(0);
         }
     }
 
@@ -48,6 +58,8 @@ public class TestTeleOpRobot {
     }
 
     public void test() {
-        slides.runToPosition(5,.5);
+        for (Motor m : slides.getMotors()) {
+            m.runToPosition(5, 0.5);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/mechanisms/LinearSlides.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/mechanisms/LinearSlides.kt
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.hardware.wrappers.Motor
 
 
 class LinearSlides(vararg motors: Motor) {
-    private val motors: List<Motor>
+    public val motors: List<Motor>
 
     /**
      * NOTE: MOTORS MUST BE SET TO THE CORRECT DIRECTION BEFORE CONSTRUCTING LINEAR SLIDES
@@ -42,7 +42,6 @@ class LinearSlides(vararg motors: Motor) {
     fun upAction() : Action {
         return object : Action {
             var initialized = false
-            var arrived = false
 
             /**
              * Runs a single uninterruptible block. Returns true if the action should run again and false if it has completed.
@@ -68,7 +67,6 @@ class LinearSlides(vararg motors: Motor) {
     fun downAction() : Action {
         return object : Action {
             var initialized = false
-            var arrived = false
 
             /**
              * Runs a single uninterruptible block. Returns true if the action should run again and false if it has completed.
